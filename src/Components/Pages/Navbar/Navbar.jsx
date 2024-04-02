331beb07c868fd8f511a2c22308b46fb37a09a88
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-02 06:04:56 +0600

    navbar updated

--- src/Components/Pages/Navbar/Navbar.jsx
+++ src/Components/Pages/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <div className="left flex  justify-center items-center gap-20">
 
                     <div>
-                        <Link className="flex items-center justify-center gap-3 text-xl font-semibold">
+                        <Link to={'/'} className="flex items-center justify-center gap-3 text-xl font-semibold hover:scale-[1.05] transition-all duration-300">
                             <img className="w-[40px] h-[40px]" src={logo} alt="" />
                             <span>HomeState</span>
                         </Link>
@@ -18,10 +18,10 @@ const Navbar = () => {
 
                     <div>
                         <ul className="flex items-center text-xl font-medium gap-6">
-                            <li><NavLink to={'/'}>Home</NavLink></li>
-                            <li><NavLink to={'/home'}>About</NavLink></li>
-                            <li><NavLink to={'/'}>Contact</NavLink></li>
-                            <li><NavLink to={'/'}>Agents</NavLink></li>
+                            <li className="hover:scale-[1.1] transition-all duration-300"><NavLink to={'/'}>Home</NavLink></li>
+                            <li className="hover:scale-[1.1] transition-all duration-300"><NavLink to={'/'}>About</NavLink></li>
+                            <li className="hover:scale-[1.1] transition-all duration-300"><NavLink to={'/'}>Contact</NavLink></li>
+                            <li className="hover:scale-[1.1] transition-all duration-300"><NavLink to={'/'}>Agents</NavLink></li>
                         </ul>
                     </div>
                 </div>
@@ -29,7 +29,7 @@ const Navbar = () => {
                 <div className="right">
                     <div>
                         <ul className="flex justify-center items-center font-medium gap-5">
-                            <li className=" btn btn-md capitalize"><Link to={'/'}>Sign In</Link></li>
+                            <li className=" btn btn-md capitalize "><Link to={'/'}>Sign In</Link></li>
                             <li className=" btn btn-md bg-yellow-300 capitalize"><Link to={'/'}>Sign Up</Link></li>
                         </ul>
                     </div>
