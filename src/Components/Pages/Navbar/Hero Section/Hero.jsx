17ed3e607165665cdb68561aaef01cb2a1badd6e
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-03 19:18:15 +0600

    hero section updated

--- src/Components/Pages/Navbar/Hero Section/Hero.jsx
+++ src/Components/Pages/Navbar/Hero Section/Hero.jsx
@@ -10,34 +10,34 @@ const Hero = () => {
                 <div className="textContainer w-[60%]">
 
                     <div className="px-10">
-                        <h1 className="text-5xl font-extrabold text-primary my-4">Find Real Estate & Get Your Dream Place</h1>
-                        <p className="text-justify font-normal  text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus, nostrum numquam vitae incidunt eum placeat. Veniam earum voluptas alias, consectetur fuga praesentium incidunt ad, magnam iste reiciendis accusantium nobis.</p>
+                        <div>
+                            <h1 className="text-3xl md:text-5xl lg:text-6xl font-extrabold text-primary my-4">Find Real Estate & Get Your Dream Place</h1>
+                            <p className="text-base md:text-lg text-justify font-normal text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi doloribus, nostrum numquam vitae incidunt eum placeat. Veniam earum voluptas alias, consectetur fuga praesentium incidunt ad, magnam iste reiciendis accusantium nobis.</p>
+                        </div>
+
 
                         <div>
                             <SearchBar></SearchBar>
                         </div>
 
-                        <div className="flex justify-between items-center">
-                            <div>
+                        <div className="flex flex-wrap justify-between items-center md:flex-row">
+                            <div className="w-full md:w-1/3 mb-4 md:mb-0">
                                 <h1 className="text-4xl font-extrabold">16+</h1>
                                 <h2 className="text-2xl font-normal text-gray-600 ">Years Of Experience</h2>
                             </div>
 
-                            <div>
+                            <div className="w-full md:w-1/3 mb-4 md:mb-0">
                                 <h1 className="text-4xl font-extrabold">200</h1>
                                 <h2 className="text-2xl font-normal text-gray-600 ">Award Gained</h2>
                             </div>
 
-                            <div>
+                            <div className="w-full md:w-1/3">
                                 <h1 className="text-4xl font-extrabold">1200+</h1>
                                 <h2 className="text-2xl font-normal text-gray-600 ">Property Ready</h2>
                             </div>
                         </div>
 
                     </div>
-
-
-
                 </div>
                 {/* text container */}
 
