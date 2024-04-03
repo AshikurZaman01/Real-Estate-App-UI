5637f44c7e9a3569d2b6688e5920144bb67f3acd
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-03 16:05:40 +0600

    updated searchbar

--- src/Components/Pages/Navbar/Hero Section/SearchBar.jsx
+++ src/Components/Pages/Navbar/Hero Section/SearchBar.jsx
@@ -2,32 +2,20 @@ import { useState } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 const SearchBar = () => {
 
-    const types = ['Buy', "Rent"];
+    const btnType = ["Buy", "Rent"];
+    const [type, setType] = useState("Buy");
 
-    const [type, setType] = useState('Buy')
-    const [location, setLocation] = useState('')
-    const [minPrice, setMinPrice] = useState(0)
-    const [maxPrice, setMaxPrice] = useState(0)
 
-    const switchType = (val) => {
-
-        setType(val)
-
-    }
 
     return (
         <div className="my-5 b">
 
-            <div className="type ">
-                {types.map((typeOption, indx) => (
-                    <button
-                        key={indx}
-                        className={` ${type === typeOption ? 'bg-black text-white' : ' border border-gray-400'}`}
-                        onClick={() => switchType(typeOption)}
-                    >
-                        {typeOption}
-                    </button>
-                ))}
+            <div className="type">
+                {
+                    btnType.map((btn, indx) => (
+                        <button key={indx} onClick={() => setType(btn)} className={`${btn === type ? 'bg-black text-white ' : "border border-gray-300 "}`}>{btn}</button>
+                    ))
+                }
             </div>
 
             <form>
